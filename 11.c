0a5eda648fa1a92a1b1fe7998b02eada6214e72f
DhanalakshmiKona <120380478+DhanalakshmiKona@users.noreply.github.com>
2023-01-11 17:53:34 +0530

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+/*
+  int a;
+  char c,d,e ;
+  printf("enter a character");
+    fflush(stdin);
+  scanf("%c",&c);
+    fflush(stdin);
+  scanf("%c",&d);
+    fflush(stdin);
+  scanf("%c",&e);
+  printf("  c=%c d=%c  e=%c ",a,c,d,e);*/
+  
+  
+  
+  
+  int a= 4;
+  int b=23,c=76;
+  int *p=&a,*e=&b;
+  printf("%d   %d",&a,&b);
+  }
